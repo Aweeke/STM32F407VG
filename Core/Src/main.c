a11b93c12eba2893930d06688546c7a860b1420c
Aweeke <aweekw@gmail.com>
2021-03-03 12:54:37 +0300

    v4.1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -100,8 +100,9 @@ int main(void)
   MX_ADC1_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-
-	HAL_ADC_Start_DMA(&hadc1,(uint32_t*)&buffer,4);
+	
+	uint16_t mas[900],j;
+  HAL_ADC_Start_DMA(&hadc1,(uint32_t*)&adc,4);	
 	
   /* USER CODE END 2 */
 
@@ -109,15 +110,29 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+		while (j<900)
+			{
+				for (;j<sizeof(mas);j++)
+				{
     /* USER CODE END WHILE */
-    HAL_ADC_Start(&hadc1);// start ADC
-		HAL_ADC_PollForConversion(&hadc1,100); //waitig for ADC
-		adc = HAL_ADC_GetValue(&hadc1);
-		HAL_ADC_Stop(&hadc1);
-		snprintf(trans_str, 1000, " ADC = %d\n\r", buffer );
-		//snprintf(trans_str, 63, " ADC = %d \n", adc );
+		  HAL_ADC_Start(&hadc1);// start ADC
+		  HAL_ADC_PollForConversion(&hadc1,100); //waitig for ADC
+		  adc = HAL_ADC_GetValue(&hadc1);
+			mas[j] = adc;
+					//Buffer[i] = HAL_ADC_GetValue(&hadc1);
+		  HAL_ADC_Stop(&hadc1);
+				}
+		}
+			//for (j=1;j<sizeof(mas);j++)
+			//{
+				snprintf(trans_str,4000,"Buffer = %d",mas[j]);
+	  //printf("%d", Buffer[i] );
+			//}
 		HAL_UART_Transmit(&huart2, (uint8_t*)trans_str, strlen(trans_str), 1000);
 		HAL_Delay(500);
+		
+			//HAL_UART_Transmit(&huart2,(uint8_t*)buf,sprintf(buf,"ch1 = %d\n\r",adc.ch1),1000);
+		  //HAL_Delay(500);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -139,9 +154,8 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -151,7 +165,7 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
@@ -184,7 +198,7 @@ static void MX_ADC1_Init(void)
   hadc1.Instance = ADC1;
   hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ScanConvMode = ENABLE;
   hadc1.Init.ContinuousConvMode = ENABLE;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
